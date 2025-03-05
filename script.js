34663815d07d3d982cfb20fa1b68d888b2654bfc
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-05 23:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let strong=document.getElementsByTagName("strong")
 function highlight() {
     //Write your code here
+    for(let i=0;i<strong.length;i++){
+        strong[i].style.color="rgb(0, 128, 0)";
+    }
 
 
 }
@@ -7,6 +11,7 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
-    
+    for(let i=0;i<strong.length;i++){
+        strong[i].style.color="rgb(0, 0, 0)";
+    }
 }
